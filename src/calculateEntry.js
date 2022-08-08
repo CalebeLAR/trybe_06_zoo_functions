8ea7eb988a2a4d28192c64fe1ec399b80499ec08
CalebeLAR <ca6ele@gmail.com>
2022-08-08 14:59:08 -0400

    requisito 8 testado e concluído

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  // const numChilds = entrants.filter((element) => (element.age < 18)).length;
+  const numChilds = entrants.reduce((sumAges, object) => (
+    (object.age < 18) ? sumAges + 1 : sumAges), 0);
+  // const numAdults = entrants.filter((object) => (object.age >= 18 && object.age < 50)).length;
+  const numAdults = entrants.reduce((sumAges, object) => (
+    (object.age >= 18 && object.age < 50) ? sumAges + 1 : sumAges), 0);
+  // const numSeniors = entrants.filter((object) => (object.age >= 50)).length;
+  const numSeniors = entrants.reduce((sumAges, object) => (
+    (object.age >= 50) ? sumAges + 1 : sumAges), 0);
+  return { child: numChilds, adult: numAdults, senior: numSeniors };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui.
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+
+  const { prices: { child: priceChild, adult: priceAdult, senior: priceSenior } } = data;
+  const reportEntrants = countEntrants(entrants);
+  const { child, adult, senior } = reportEntrants;
+  return child * priceChild + adult * priceAdult + senior * priceSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
