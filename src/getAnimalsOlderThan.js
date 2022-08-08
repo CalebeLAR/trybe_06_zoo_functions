f2c3075f37c28c2bc1906d1984c0dd3754f2dfcf
CalebeLAR <ca6ele@gmail.com>
2022-08-07 21:59:22 -0400

    requisito 2 testado e concluído

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const especie = species.filter((objAnimal) => (objAnimal.name === animal));
+  const bolean = especie[0].residents.every((individuo) => (individuo.age >= age));
+  return bolean;
 }
 
 module.exports = getAnimalsOlderThan;
